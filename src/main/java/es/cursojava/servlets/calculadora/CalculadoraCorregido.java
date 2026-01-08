4e2d111ba549200e69a135415bce6146295f1740
Elia <elia@mail.com>
2026-01-08 18:40:39 +0100

    calculadora attempt 2

---
+++ src/main/java/es/cursojava/servlets/calculadora/CalculadoraCorregido.java
@@ -0,0 +1,48 @@
+package es.cursojava.servlets.calculadora;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/CalculadoraCorregido")
+public class CalculadoraCorregido extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		super.init(config);
+	}
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(arg0, arg1);
+	}
+
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(arg0, arg1);
+	}
+
+	
+}
--- src/main/java/es/cursojava/servlets/calculadora/CalculadoraServlet.java
+++ src/main/java/es/cursojava/servlets/calculadora/CalculadoraServlet.java
@@ -1,22 +1,65 @@
 package es.cursojava.servlets.calculadora;
 
 import java.io.IOException;
+import java.util.Random;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
-@WebServlet
+@WebServlet("/Calculadora1")
 public class CalculadoraServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
 
+	//puts numbers into the session
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException{
+	Random r = new Random();
+	int num1 = r.nextInt(100)+1;
+	int num2 = r.nextInt(100)+1;
+		
+	//saves numbers in session
+	HttpSession session = request.getSession();
+	session.setAttribute("num1", num1);
+	session.setAttribute("num2", num2);
+	
+	//Go to HTML page, so it calculates the numbers before showing the page
+	request.getRequestDispatcher("Calculadora1.jsp").forward(request, response);
+	}
+	
+	
+	//takes numbers from session and puts them on page
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException{
-			
 		
+		//Gets numbers from session
+		HttpSession session = request.getSession();
+		int num1 =(int) session.getAttribute("num1");
+		int num2 =(int) session.getAttribute("num2");
+		
+		//Get selected operation
+		String operation = request.getParameter("operacion");
+		int result = 0;
+		
+		if (operation.equals("suma")) {
+			result = num1 + num2;
+		}
+		if(operation.equals("resta")) {
+			result = num1 - num2;
+		}
+		if(operation.equals("multiplicacion")) {
+			result = num1*num2;
+		}
+		
+		// Send result to HTML
+		request.setAttribute("resultado", result); //Stores the calculation result in the request so another page can read it. only 1 use
+		request.getRequestDispatcher("");
 	}
+	
 }
